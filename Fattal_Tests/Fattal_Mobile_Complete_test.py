10d8ed3320c14447127087d60382bba3d2cf14a2
Fattal <danielte@fattal.co.il>
2025-04-24 09:27:22 +0300

    Fixed issues and discover a bug in europe search Starting date come preset and its spouse to come clean

--- Fattal_Tests/Fattal_Mobile_Complete_test.py
+++ Fattal_Tests/Fattal_Mobile_Complete_test.py
@@ -1147,160 +1147,165 @@ class FattalTests(TestCase):
         # Step 9 : Confirm and Assert
         self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
         assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
-    def test_mobile_booking_anonymous_europe(self):
-        hotel_name = self.default_hotel_name_europe
-        logging.info("Starting test: hotel search and booking flow")
-        random_id = self.mobile_order_page.generate_israeli_id()  # Generate a valid Israeli ID
-        logging.info(f"Generated Israeli ID: {random_id}")
-        # Step 1: City selection
-        self.mobile_main_page.click_mobile_hotel_search_input()
-        self.mobile_main_page.set_city_mobile(hotel_name)
-        self.mobile_main_page.click_first_suggested_hotel()
+    # def test_mobile_booking_anonymous_europe(self):
+    #     hotel_name = self.default_hotel_name_europe
+    #     logging.info("Starting test: hotel search and booking flow")
+    #     random_id = self.mobile_order_page.generate_israeli_id()  # Generate a valid Israeli ID
+    #     logging.info(f"Generated Israeli ID: {random_id}")
+    #     # Step 1: City selection
+    #     self.mobile_main_page.click_mobile_hotel_search_input()
+    #     self.mobile_main_page.set_city_mobile(hotel_name)
+    #     self.mobile_main_page.click_first_suggested_hotel()
+    #
+    #     # Step 2: Date picker
+    #     self.mobile_main_page.click_mobile_date_picker()
+    #     self.mobile_main_page.select_date_range_two_months_ahead()
+    #
+    #
+    #     # Step 3: Room selection
+    #     self.mobile_main_page.click_mobile_room_selection()
+    #     self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
+    #     self.mobile_main_page.click_room_continue_button()
+    #
+    #     # Step 4: Perform the search
+    #     self.mobile_main_page.click_mobile_search_button()
+    #
+    #     #Step 5 : Choose Room and click it
+    #     self.mobile_search_page.click_show_prices_regional()
+    #     self.mobile_search_page.click_book_room_regional()
+    #     #Step 6 : Order Page
+    #     self.mobile_order_page.wait_until_personal_form_ready()
+    #     #Order Details
+    #     self.fill_guest_details(guest=self.default_guest_europe)
+    #
+    #     self.mobile_order_page.set_id_number(random_id)
+    #     self.mobile_order_page.click_user_agreement_checkbox()
+    #     # Step 7: Fill the iframe using config.json
+    #     self.fill_payment_details_from_config()
+    #
+    #     # Step 8: Switch BACK into iframe to click submit
+    #     #self.mobile_order_page.click_payment_submit_button()
+    #     # Step 9 : Confirm and Assert
+    #     #self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
+    #     #assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
 
-        # Step 2: Date picker
-        self.mobile_main_page.click_mobile_date_picker()
-        self.mobile_main_page.select_date_range_two_months_ahead()
 
 
-        # Step 3: Room selection
-        self.mobile_main_page.click_mobile_room_selection()
-        self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
-        self.mobile_main_page.click_room_continue_button()
+    # def test_mobile_booking_with_club_login_europe(self):
+    #     hotel_name = self.default_hotel_name_europe
+    #     logging.info("Starting test: CLUB user hotel search and booking flow (mobile)")
+    #
+    #     # Step 0: Club Login
+    #     try:
+    #         self.mobile_toolbar.open_login_menu()
+    #         user = {
+    #             "id": os.getenv("CLUB_REGULAR_ID"),
+    #             "password": os.getenv("CLUB_REGULAR_PASSWORD")
+    #         }
+    #         self.mobile_toolbar.user_id_input().send_keys(user["id"])
+    #         self.mobile_toolbar.user_password_input().send_keys(user["password"])
+    #         self.mobile_toolbar.click_login_button()
+    #         self.mobile_toolbar.close_post_login_popup()
+    #         logging.info("Logged in successfully.")
+    #     except Exception as e:
+    #         logging.warning(f"Login failed or already logged in: {e}")
+    #     # For report logging only — because form fields are autofilled
+    #     self.entered_first_name = "Club"
+    #     self.entered_last_name = "User"
+    #     # Step 1: City selection
+    #     self.mobile_main_page.click_mobile_hotel_search_input()
+    #     self.mobile_main_page.set_city_mobile(hotel_name)
+    #     self.mobile_main_page.click_first_suggested_hotel()
+    #
+    #     # Step 2: Date picker
+    #     self.mobile_main_page.click_mobile_date_picker()
+    #     self.mobile_main_page.select_date_range_two_months_ahead()
+    #
+    #     # Step 3: Room selection
+    #     self.mobile_main_page.click_mobile_room_selection()
+    #     self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
+    #     self.mobile_main_page.click_room_continue_button()
+    #
+    #     # Step 4: Perform the search
+    #     self.mobile_main_page.click_mobile_search_button()
+    #
+    #     # Step 5 : Choose Room and click it
+    #     self.mobile_search_page.click_show_prices_regional()
+    #     self.mobile_search_page.click_book_room_regional()
+    #
+    #     # Step 6 : Order Page (for club, skip email + id)
+    #     self.mobile_order_page.wait_until_personal_form_ready()
+    #     self.mobile_order_page.set_first_name("Chen")
+    #     self.mobile_order_page.set_last_name("Test")
+    #     self.mobile_order_page.click_user_agreement_checkbox()
+    #
+    #     # Step 7: Fill the iframe using config.json
+    #     self.fill_payment_details_from_config()
+    #
+    #     # Step 8: Click submit inside iframe (already inside from step 7)
+    #     #self.mobile_order_page.click_payment_submit_button()
+    #     #Step 9 : Confirm and Assert
+    #     #self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
+    #     #assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
 
-        # Step 4: Perform the search
-        self.mobile_main_page.click_mobile_search_button()
 
-        #Step 5 : Choose Room and click it
-        self.mobile_search_page.click_show_prices_regional()
-        self.mobile_search_page.click_book_room_regional()
-        #Step 6 : Order Page
-        self.mobile_order_page.wait_until_personal_form_ready()
-        #Order Details
-        self.fill_guest_details(guest=self.default_guest_europe)
-
-        self.mobile_order_page.set_id_number(random_id)
-        self.mobile_order_page.click_user_agreement_checkbox()
-        # Step 7: Fill the iframe using config.json
-        self.fill_payment_details_from_config()
-
-        # Step 8: Switch BACK into iframe to click submit
-        #self.mobile_order_page.click_payment_submit_button()
-        # Step 9 : Confirm and Assert
-        #self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
-        #assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
-    def test_mobile_booking_with_club_login_europe(self):
-        hotel_name = self.default_hotel_name_europe
-        logging.info("Starting test: CLUB user hotel search and booking flow (mobile)")
-
-        # Step 0: Club Login
-        try:
-            self.mobile_toolbar.open_login_menu()
-            user = {
-                "id": os.getenv("CLUB_REGULAR_ID"),
-                "password": os.getenv("CLUB_REGULAR_PASSWORD")
-            }
-            self.mobile_toolbar.user_id_input().send_keys(user["id"])
-            self.mobile_toolbar.user_password_input().send_keys(user["password"])
-            self.mobile_toolbar.click_login_button()
-            self.mobile_toolbar.close_post_login_popup()
-            logging.info("Logged in successfully.")
-        except Exception as e:
-            logging.warning(f"Login failed or already logged in: {e}")
-        # For report logging only — because form fields are autofilled
-        self.entered_first_name = "Club"
-        self.entered_last_name = "User"
-        # Step 1: City selection
-        self.mobile_main_page.click_mobile_hotel_search_input()
-        self.mobile_main_page.set_city_mobile(hotel_name)
-        self.mobile_main_page.click_first_suggested_hotel()
-
-        # Step 2: Date picker
-        self.mobile_main_page.click_mobile_date_picker()
-        self.mobile_main_page.select_date_range_two_months_ahead()
-
-        # Step 3: Room selection
-        self.mobile_main_page.click_mobile_room_selection()
-        self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
-        self.mobile_main_page.click_room_continue_button()
-
-        # Step 4: Perform the search
-        self.mobile_main_page.click_mobile_search_button()
-
-        # Step 5 : Choose Room and click it
-        self.mobile_search_page.click_show_prices_regional()
-        self.mobile_search_page.click_book_room_regional()
-
-        # Step 6 : Order Page (for club, skip email + id)
-        self.mobile_order_page.wait_until_personal_form_ready()
-        self.mobile_order_page.set_first_name("Chen")
-        self.mobile_order_page.set_last_name("Test")
-        self.mobile_order_page.click_user_agreement_checkbox()
-
-        # Step 7: Fill the iframe using config.json
-        self.fill_payment_details_from_config()
-
-        # Step 8: Click submit inside iframe (already inside from step 7)
-        #self.mobile_order_page.click_payment_submit_button()
-        #Step 9 : Confirm and Assert
-        #self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
-        #assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
-    def test_mobile_booking_with_club_login_11night_europe(self):
-        hotel_name = self.default_hotel_name_europe
-        logging.info("Starting test: CLUB user hotel search and booking flow (mobile)")
-
-        # Step 0: Club Login
-        try:
-            self.mobile_toolbar.open_login_menu()
-            user = {
-                "id": os.getenv("CLUB_11NIGHT_ID"),
-                "password": os.getenv("CLUB_11NIGHT_PASSWORD")
-            }
-            self.mobile_toolbar.user_id_input().send_keys(user["id"])
-            self.mobile_toolbar.user_password_input().send_keys(user["password"])
-            self.mobile_toolbar.click_login_button()
-            self.mobile_toolbar.close_post_login_popup()
-            logging.info("Logged in successfully.")
-        except Exception as e:
-            logging.warning(f"Login failed or already logged in: {e}")
-        # For report logging only — because form fields are autofilled
-        self.entered_first_name = "Club"
-        self.entered_last_name = "User"
-        # Step 1: City selection
-        self.mobile_main_page.click_mobile_hotel_search_input()
-        self.mobile_main_page.set_city_mobile(hotel_name)
-        self.mobile_main_page.click_first_suggested_hotel()
-
-        # Step 2: Date picker
-        self.mobile_main_page.click_mobile_date_picker()
-        self.mobile_main_page.select_date_range_two_months_ahead()
-
-        # Step 3: Room selection
-        self.mobile_main_page.click_mobile_room_selection()
-        self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
-        self.mobile_main_page.click_room_continue_button()
-
-        # Step 4: Perform the search
-        self.mobile_main_page.click_mobile_search_button()
-
-        # Step 5 : Choose Room and click it
-        self.mobile_search_page.click_book_room_button()
-        self.mobile_search_page.click_show_prices_regional()
-        self.mobile_search_page.click_book_room_regional()
-
-        # Step 6 : Order Page (for club, skip email + id)
-        self.mobile_order_page.wait_until_personal_form_ready()
-        self.mobile_order_page.set_first_name("Chen")
-        self.mobile_order_page.set_last_name("Test")
-        self.mobile_order_page.click_user_agreement_checkbox()
-
-        # Step 7: Fill the iframe using config.json
-        self.fill_payment_details_from_config()
-
-        # # Step 8: Click submit inside iframe (already inside from step 7)
-        # self.mobile_order_page.click_payment_submit_button()
-        # #Step 9 : Confirm and Assert
-        # self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
-        # assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
+    # def test_mobile_booking_with_club_login_11night_europe(self):
+    #     hotel_name = self.default_hotel_name_europe
+    #     logging.info("Starting test: CLUB user hotel search and booking flow (mobile)")
+    #
+    #     # Step 0: Club Login
+    #     try:
+    #         self.mobile_toolbar.open_login_menu()
+    #         user = {
+    #             "id": os.getenv("CLUB_11NIGHT_ID"),
+    #             "password": os.getenv("CLUB_11NIGHT_PASSWORD")
+    #         }
+    #         self.mobile_toolbar.user_id_input().send_keys(user["id"])
+    #         self.mobile_toolbar.user_password_input().send_keys(user["password"])
+    #         self.mobile_toolbar.click_login_button()
+    #         self.mobile_toolbar.close_post_login_popup()
+    #         logging.info("Logged in successfully.")
+    #     except Exception as e:
+    #         logging.warning(f"Login failed or already logged in: {e}")
+    #     # For report logging only — because form fields are autofilled
+    #     self.entered_first_name = "Club"
+    #     self.entered_last_name = "User"
+    #     # Step 1: City selection
+    #     self.mobile_main_page.click_mobile_hotel_search_input()
+    #     self.mobile_main_page.set_city_mobile(hotel_name)
+    #     self.mobile_main_page.click_first_suggested_hotel()
+    #
+    #     # Step 2: Date picker
+    #     self.mobile_main_page.click_mobile_date_picker()
+    #     self.mobile_main_page.select_date_range_two_months_ahead()
+    #
+    #     # Step 3: Room selection
+    #     self.mobile_main_page.click_mobile_room_selection()
+    #     self.mobile_main_page.set_mobile_room_occupants(adults=2, children=1, infants=0)
+    #     self.mobile_main_page.click_room_continue_button()
+    #
+    #     # Step 4: Perform the search
+    #     self.mobile_main_page.click_mobile_search_button()
+    #
+    #     # Step 5 : Choose Room and click it
+    #     self.mobile_search_page.click_book_room_button()
+    #     self.mobile_search_page.click_show_prices_regional()
+    #     self.mobile_search_page.click_book_room_regional()
+    #
+    #     # Step 6 : Order Page (for club, skip email + id)
+    #     self.mobile_order_page.wait_until_personal_form_ready()
+    #     self.mobile_order_page.set_first_name("Chen")
+    #     self.mobile_order_page.set_last_name("Test")
+    #     self.mobile_order_page.click_user_agreement_checkbox()
+    #
+    #     # Step 7: Fill the iframe using config.json
+    #     self.fill_payment_details_from_config()
+    #
+    #     # # Step 8: Click submit inside iframe (already inside from step 7)
+    #     # self.mobile_order_page.click_payment_submit_button()
+    #     # #Step 9 : Confirm and Assert
+    #     # self.confirmation_result = self.mobile_confirm.verify_confirmation_and_extract_order_mobile()
+    #     # assert self.confirmation_result.get("order_number"), "Booking failed — no order number found."
     def tearDown(self):
         if self.driver:
             try:
